4eab046e3815bad2c2f396d91d97e612e25450d1
Osvaldo943 <osvaldodesousa9432@gmail.com>
2024-01-13 03:37:11 +0000

    feat: finish tasks screen

--- src/components/categories/styles.tsx
+++ src/components/categories/styles.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   width: 100%;
   height: 200px;
-  padding: 0 16px;
+  padding-left: 16px;
 `;
 export const CategoriesContainer = styled.View`
   width: 100%;
